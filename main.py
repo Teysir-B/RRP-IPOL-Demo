92e7a8047c065b305ebb5cd479c2e970852591e1
Teysir-B <81953038+Teysir-B@users.noreply.github.com>
2023-02-03 05:08:30 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+import whisper
+
+def main(audio, language):
+    model = whisper.load_model("base")
+    # Set options
+    options = dict(language = language)
+    transcribe_options = dict(task = "transcribe", **options)
+    result = model.transcribe(audio, **transcribe_options)
+    print(result["text"])
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--audio", type = str, required=True)
+    parser.add_argument("--language", type = str, required=True)
+
+    args = parser.parse_args()
+    main(args.audio, args.language)
