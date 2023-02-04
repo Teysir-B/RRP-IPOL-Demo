e4c23741e39a841175fd339c47d00ed83af48fc8
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-04 18:57:49 +0100

    Test

--- main.py
+++ main.py
@@ -15,4 +15,5 @@ if __name__ == "__main__":
     parser.add_argument("--language", type = str, required=True)
 
     args = parser.parse_args()
-    main(args.audio, args.language)
+    print(args.language)
+    #main(args.audio, args.language)
