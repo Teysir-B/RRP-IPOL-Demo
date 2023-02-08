a49043ede8ec6d03a303cf7522d3255cd87f223a
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-08 16:57:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
-#import whisper
 import os
 import iio
 import numpy as np
 from scipy.io import wavfile
-from scipy.io import wavfile
 
+# if you need to access a file next to the source code, use the variable ROOT
+# for example:
+#    torch.load(os.path.join(ROOT, 'weights.pth'))
 ROOT = os.path.dirname(os.path.realpath(__file__))
 
-def main(audio, language):
-    print(audio)
-    
-    #model = whisper.load_model("base")
-    # Set options
-    #options = dict(language = language)
-    #transcribe_options = dict(task = "transcribe", **options)
-    #result = model.transcribe(audio, **transcribe_options)
-    #print(result["text"])
+def main(input, output, sigma):
+
+    samplerate, data = wavfile.read(input)
+    print(samplerate, data)
+    wavfile.write(output, samplerate//2, data.astype(np.int16))
 
 if __name__ == "__main__":
-    print("Blablabla")
     import argparse
-    
     parser = argparse.ArgumentParser()
-    parser.add_argument("--audio", type = str, required=True)
-    parser.add_argument("--language", type = str, required=True)
-
-    args = parser.parse_args()
+    parser.add_argument("--audio", type=str, required=True)
+    parser.add_argument("--language", type=float, required=True)
+    #parser.add_argument("--output", type=str, required=True)
     
-    main(args.audio, args.language)
+    args = parser.parse_args()
+    print("Blablabla")
+    #main(args.input, args.output, args.sigma)
