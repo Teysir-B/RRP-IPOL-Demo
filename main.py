ab0e4bce5fce9e46d96d39e1347e3ffa14104ee2
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-09 17:03:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def main(audio_file, language):
         new_len = int(len(audio)*(RATE/datarate))
         audio = signal.resample(audio, new_len)
     audio = whisper.pad_or_trim(audio)
-    #!gdown "https://drive.google.com/uc?id=12v5I212oqXDvCsKnU5cSnbFt3eT28sax&confirm=t"
+    gdown "https://drive.google.com/uc?id=12v5I212oqXDvCsKnU5cSnbFt3eT28sax&confirm=t"
     #checkpoint_path = "./tiny_multilanguage.ckpt"
     #model = whisper.load_model(checkpoint_path)
     model = whisper.load_model("tiny")
