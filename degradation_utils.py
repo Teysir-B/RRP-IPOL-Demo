c28ce066f2250ac346d05f630be68aeb0b13ba3f
Helene-Maxcici <119662709+Helene-Maxcici@users.noreply.github.com>
2023-02-23 14:07:03 +0100

    add degradation, HF whisper

---
+++ degradation_utils.py
@@ -0,0 +1,55 @@
+import time
+from typing import List
+import numpy as np
+import audio_degrader as ad
+
+def compose_degradations(add_noise, snr, impulse_response, wet_level):
+  """ Compose degradation json file from parameters """
+  list_degradations = []
+  # Additive noise
+  if len(add_noise) != 0:
+    list_degradations.append(f"mix,{add_noise},{snr}")
+  # Convolution + Reverberation
+  if len(impulse_response) != 0:
+    list_degradations.append(f"convolution,{impulse_response},{wet_level}")
+  # ...
+  if len(list_degradations) !=0 :
+    print("\nDegradation Composition:")
+    for d in list_degradations:
+      print(f"\t{d}")
+  
+  return list_degradations
+
+def prepare_audio(samples):
+    # Normalize (+other if needed)
+    if np.abs(samples).max() > 1:
+      samples = samples.astype(np.float32)
+      rms_samples = np.sqrt(np.sum(np.power(samples, 2)))
+      samples = samples/rms_samples
+    return samples
+
+def apply_degradation(degradation: List[str], samples, 
+                      sample_rate_in: int = 16e3, verbose=0):
+  """
+  Function to apply degradations on one audio samples.
+  Inputs:
+  - degradation: List of strings. String format name_degradation,param1,param2.
+  - samples: Numpy array of samples.
+  - sample_rate_in: Sample rate of the audio. 
+                    If not equal to 16k Hz, will be resampled.
+  """
+  st = time.time()
+  # Prep audio
+  audio = ad.AudioArray(samples_in = samples, 
+                      sample_rate_in = sample_rate_in,
+                      sample_rate_process= 16e3, # Default rate
+                      bits = 64) # float16 for Whisper
+  # Loop over degradations and apply
+  degradation = ad.ParametersParser.parse_degradations_args(degradation)
+  for d in degradation:
+    audio.apply_degradation(d)
+  et = time.time()
+  if verbose>0:
+    print(f"\nApplied degradations in {et-st:.3f} seconds.")
+  return audio.samples, int(audio.sample_rate)
+
--- main.py
+++ main.py
@@ -1,54 +1,122 @@
 import os
 import iio
+import time
 import gdown
+import json
 import numpy as np
 
 from scipy.io import wavfile
 from scipy import signal
+from degradation_utils import (prepare_audio, apply_degradation,
+                              compose_degradations)
 
-import whisper
+# Whisper
+from transformers import (WhisperForConditionalGeneration, 
+                          WhisperFeatureExtractor, WhisperTokenizer, 
+                          WhisperTokenizer, WhisperProcessor)
 
 # if you need to access a file next to the source code, use the variable ROOT
 # for example:
 #    torch.load(os.path.join(ROOT, 'weights.pth'))
+
 ROOT = os.path.dirname(os.path.realpath(__file__))
-RATE = 16e3
-def main(audio_file, language, output_audio, noisy_audio, sigma):
-    # Load audio
-    datarate, audio = wavfile.read(audio_file)
-    if len(audio.shape) == 2:
-        # Stereo to Mono
-        audio = audio.mean(axis=1)
-    # Normalize audio
-    audio = audio / audio.max() 
-    audio = audio.astype(np.float16)
-    # Resample
-    if datarate != RATE:
-        new_len = int(len(audio)*(RATE/datarate))
-        audio = signal.resample(audio, new_len)
-    audio = whisper.pad_or_trim(audio)
-    wavfile.write(output_audio, int(RATE), audio)
-    audio += np.random.normal(0, sigma, *audio.shape)
-    wavfile.write(noisy_audio, int(RATE), audio)
-    checkpoint_path = "./tiny_multilanguage.ckpt"
-    gdown.download(url = "https://drive.google.com/uc?id=12v5I212oqXDvCsKnU5cSnbFt3eT28sax&confirm=t", 
-                   output = checkpoint_path, quiet=False)
+
+# Constants
+RATE = 16e3 # Default rate of whisper
+
+def load_finetuned(language="French"):
+  """Download finetuned weights from drive"""
+  if not os.path.isdir("./pretrained"):
+    os.mkdir("./pretrained")
+  st = time.time()
+  # Load dictionary of ids
+  with open("./finetuned_models.json") as file:
+    dict_finetuned = json.load(file)  
+  # Download 
+  for f, id in dict_finetuned[language].items():
+    gdown.download(f"https://drive.google.com/uc?id={id}&confirm=t",
+                 output=f"./pretrained/{f}")
+  et = time.time()
+  print(f"\nDownloaded finetuned weights in {et-st:.3f} seconds.")
+
+def main(audio_in, audio_out, # audio files
+          language, finetuned, # model 
+          add_noise, snr, # degradation options
+          impulse_response, wet_level):
     
-    model = whisper.load_model(checkpoint_path)
-    #model = whisper.load_model("tiny")
-    options = dict(language = language)
-    transcribe_options = dict(task = "transcribe", **options)
-    results = model.transcribe(audio, **transcribe_options)
-    print(results["text"])
+    ## Load audio
+    sample_rate_in, samples = wavfile.read(audio_in)
+    
+    ## Preprocess audio
+    # Stereo to Mono
+    if len(samples.shape) == 2:
+        samples = samples.mean(axis=1)
+    # Resample
+    if sample_rate_in != RATE:
+        samples, sample_rate = apply_degradation(["resample,16000"], 
+                                                samples, sample_rate_in)
+        assert sample_rate == RATE
+    # Normalize
+    samples = prepare_audio(samples)
+    # Apply degradations
+    list_degradations = compose_degradations(add_noise, snr,
+                                            impulse_response, wet_level)
+    if len(list_degradations) !=0:
+      samples, sample_rate = apply_degradation(list_degradations, 
+                                                samples, RATE, verbose=1)
+    # Trim audio
+    max_len = int(RATE*30)
+    if len(samples)>max_len:
+      samples = samples[:max_len]
+    # Write processed audio
+    wavfile.write(audio_out, int(RATE), samples)
+
+    ## Load model
+    print(finetuned)  
+    if finetuned == "true":
+      try:
+        load_finetuned(language)
+      except:
+        print("\nFailed to download weights from Drive. Please refresh or try later.")
+      pretrained_path = "./pretrained"
+    else:
+      pretrained_path = "openai/whisper-tiny" 
+      print(f"\nYou haven't checked finetuned weights. " 
+            f"The pretrained model is the multilangual \"openai/whisper-tiny\".")
+
+    ## Instanciate whisper pipeline
+    # feature_extractor: 1. trim+pad 2. STFT 3. MEL cepstrum == features
+    feature_extractor = WhisperFeatureExtractor.from_pretrained(pretrained_path)
+    # tokenizer: 1. split to tokens 2. tokens to ids 3. and back
+    tokenizer = WhisperTokenizer.from_pretrained("openai/whisper-tiny",
+                                                language=language, 
+                                                task="transcribe")
+    # processor: wraps feature extractor + tokenizer 
+    processor = WhisperProcessor(feature_extractor, tokenizer)
+    model = WhisperForConditionalGeneration.from_pretrained(pretrained_path)
+
+    ## Process audio with Whisper
+    inputs = processor(samples, sampling_rate=RATE, return_tensors="pt")
+    generated_ids = model.generate(inputs=inputs.input_features)
+    transcription = processor.batch_decode(generated_ids, 
+                                            skip_special_tokens=True)[0]
+    print("\n",transcription)
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser()
-    parser.add_argument("--audio_file", type=str, required=True)
+    parser.add_argument("--audio_in", type=str, required=True)
+    parser.add_argument("--audio_out", type=str, required=True)
     parser.add_argument("--language", type=str, required=True)
-    parser.add_argument("--output_audio", type=str, required=True)
-    parser.add_argument("--noisy_audio", type=str, required=True)
-    parser.add_argument("--sigma", type=float, required=True)
+    parser.add_argument("--finetuned", type=str, required=True)
+    parser.add_argument("--add_noise", type=str, required=True)
+    parser.add_argument("--snr", type=float, required=True)
+    parser.add_argument("--impulse_response", type=str, required=True)
+    parser.add_argument("--wet_level", type=float, required=True)
+
     
     args = parser.parse_args()
-    main(args.audio_file, args.language, args.output_audio, args.noisy_audio, args.sigma)
+    main(args.audio_in, args.audio_out, 
+          args.language, args.finetuned, 
+          args.add_noise, args.snr,
+          args.impulse_response, args.wet_level)
